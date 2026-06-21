e01ecc87d52d225e085de8900a8d4e7ea1dbc67a
Irsyadmr354 <irsyadmr354@gmail.com>
2026-06-22 01:19:31 +0700

    feat: final 1000x UI polish with drop shadows, animations, and hover scales

--- BetterDiskCleanup.Core/obj/Debug/net8.0/BetterDiskCleanup.Core.AssemblyInfo.cs
+++ BetterDiskCleanup.Core/obj/Debug/net8.0/BetterDiskCleanup.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetterDiskCleanup.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46d0151ee21ee21ac102d65da8db9809be16425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e19a1c9762fd3c529be0620e4b0aef53511d67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetterDiskCleanup.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetterDiskCleanup.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
